eaf1ef0ae7cee4dff5ce0de3ae5d12db96d2cab8
nonono <nonono@tokyo.email.ne.jp>
2025-12-09 22:04:31 +0900

    DB接続

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import json
+import os
+import psycopg2
+import boto3
 from fastapi import FastAPI
 from mangum import Mangum
 
@@ -10,6 +14,44 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "query": q}
+
+def get_db_info():
+    secret_name = os.environ["SECRET_NAME"]
+    region_name = "ap-southeast-2"
+
+    client = boto3.client("secretsmanager", region_name=region_name)
+    response = client.get_secret_value(SecretId=secret_name)
+
+    secret = json.loads(response["SecretString"])
+    return secret
+
+
+@app.get("/dbtest")
+def dbtest():
+    try:
+        secret = get_db_info()
+
+        conn = psycopg2.connect(
+            host=secret["host"],
+            dbname=secret["dbname"],
+            user=secret["user"],
+            password=secret["password"],
+            port=secret["port"]
+        )
+
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        return {
+            "status": "connected",
+            "postgres_version": version[0]
+        }
+
+    except Exception as e:
+        return {"status": "error", "detail": str(e)}
 
 # Lambda用のハンドラー
 handler = Mangum(app, api_gateway_base_path="/default")
